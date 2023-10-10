f9aa427a9bd0a1067026f9762f1b422039a716e6
joaolfp <joaolucasfp2001@gmail.com>
2023-10-10 00:06:29 -0300

    Add validation with network

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use serde::{Deserialize, Serialize};
+use std::io;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct APIResponse {
+    login: String,
+    url: String,
+    name: String,
+    followers: i16,
+    following: i16
+}
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    println!("Enter the user you want to query:");
+    let mut user = String::new();
+    io::stdin().read_line(&mut user).expect("Error typing username");
+
+    println!("---------------------------------");
+
+    let url = format!(
+        "https://api.github.com/users/{set_user}",
+        set_user = user
+    );
+
+    let response = reqwest::Client::new()
+        .get(url)
+        .header(reqwest::header::USER_AGENT, "MyInfo")
+        .send()
+        .await?;
+
+    if response.status().is_success() {
+        let result: APIResponse = response.json().await?;
+        println!("Login: {}", result.login);
+        println!("URL: {}", result.url);
+        println!("Name: {}", result.name);
+        println!("Followers: {}", result.followers);
+        println!("Following: {}", result.following);
+    } else {
+        println!("Request failed with status code: {}", response.status());
+    }
+
+    Ok(())
 }

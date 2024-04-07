bbe36468e170fe50e6e63b9ab98c5b7de518200a
joaolfp <joaolucasfp2001@gmail.com>
2024-04-06 21:44:08 -0300

    Add new tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,11 @@
-#[cfg(test)]
 mod tests {
     use tokio::test;
+    
+    #[allow(unused_imports)]
+    use crate::response;
+
+    #[allow(unused_imports)]
+    use crate::response_repo;
 
     #[test]
     async fn test_make_request() {
@@ -12,4 +17,16 @@ mod tests {
 
         assert!(response.is_ok());
     }
+
+    #[test]
+     async fn test_response() {
+        let result = response("user".to_string()).await;
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    async fn test_response_repo() {
+       let result = response_repo("user".to_string()).await;
+       assert!(result.is_ok());
+   }
 }

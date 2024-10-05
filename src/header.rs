738a32323a9490551c77dcca6db4469deaa77c54
joaolfp <joaolucasfp2001@gmail.com>
2024-10-05 02:39:26 -0300

    Setup progress bar with indicatif

--- src/header.rs
+++ src/header.rs
@@ -1,5 +1,8 @@
 use std::io;
 use colored::Colorize;
+use indicatif::ProgressBar;
+use std::thread::sleep;
+use std::time::Duration;
 
 pub fn presentation() {
     let text = r#"
@@ -25,6 +28,22 @@ pub fn header() -> String {
     println!("Enter the user you want to query:");
     let mut user = String::new();
     io::stdin().read_line(&mut user).expect("Error typing username");
-    println!("---------------------------------");
+
+    println!("\n");
+    progress_bar();
+    println!("\n");
+    
     return user
+}
+
+fn progress_bar() {
+    let total_steps = 100;
+    let progress_bar = ProgressBar::new(total_steps);
+
+    for _ in 0..total_steps {
+        progress_bar.inc(1);
+        sleep(Duration::from_millis(50));
+    }
+
+    progress_bar.finish_with_message("Done");
 }
